9607c3e20679b3c2773f1bf16e24de79ff554b03
banditopazzo <banditopazzo@gmail.com>
2019-07-18 22:26:59 +0200

    primo lancio di uno script di test

--- src/app.py
+++ src/app.py
@@ -2,17 +2,31 @@ import json
 import os
 
 CONFIG_FILE_NAME = 'config.json'
-DIR_NAME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-CONFIG_PATH = os.path.join(DIR_NAME, 'resources', CONFIG_FILE_NAME)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+CONFIG_PATH = os.path.join(BASE_DIR, 'resources', CONFIG_FILE_NAME)
+SCRIPT_FOLDER = os.path.join(BASE_DIR, 'resources', 'scripts')
 
-def readConfig():
-    with open(CONFIG_PATH) as json_file:
+def readJsonFile(file_path):
+    with open(file_path) as json_file:
         return json.load(json_file)
 
 def main():
-    config = readConfig()
-    for table_conf in config:
-        print(table_conf['script'])
+
+    # Legge il file con le configurazione delle operazioni
+    config = readJsonFile(CONFIG_PATH)
+
+    # Separa le configurazioni che utilizzano script gia pronti da quelle che richiedono la creazioni di comandi sqoop
+    ready_script_config = [x for x in config if x['script']]
+    raw_script_data = [x for x in config if not x['script']]
+
+    # Primo test di funzionamento
+    print("\nREADY:\n")
+    print(ready_script_config)
+    print('\n')
+    os.system('sh ' + os.path.join(SCRIPT_FOLDER, ready_script_config[0]['script_name']))
+    print("\nTO CREATE:\n")
+    print(raw_script_data)
+    print('\n')
 
 if __name__ == "__main__":
     main()
