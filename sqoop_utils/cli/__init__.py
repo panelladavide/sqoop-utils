7276c07ad69b7ef9be3e9c217512b28bfa1d5de5
banditopazzo <banditopazzo@gmail.com>
2019-07-23 16:15:59 +0200

    up cli

--- sqoop_utils/cli/__init__.py
+++ sqoop_utils/cli/__init__.py
@@ -41,6 +41,15 @@ def main():
     if skip_standalone and skip_custom:
         print("Why do you skip all jobs?")
         exit(0)
+
+    # Initialize CSV report file
+    current_millis = int(round(time.time() * 1000))
+    output_file_name = '.'.join(str(current_millis), 'csv')
+    OUTPUT_FILE = os.path.join(REPORTS_DIR, output_file_name)
+    csv_fieldnames = ['table_name', 'map_input_records', 'map_output_records']
+    with open(OUTPUT_FILE, mode='a+') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
     
     # Process configured sqoop jobs
     for job in config_data:
@@ -61,10 +70,17 @@ def main():
 
         child_process = subprocess.run(['sh','-c', sqoop_cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
         (m_in, m_out) = script_utils.parse_output(child_process.stdout)
-        print(table_name, m_in, m_out)
+            
+        record = {
+            "table_name": table_name,
+            "map_input_records": m_in,
+            "map_output_records": m_out
+        }
 
-    # Write the output report
-    # TODO:
+        # Write to the CSV report file
+        with open(OUTPUT_FILE, mode='a+') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writerow(record)
 
 if __name__ == "__main__":
     main()
