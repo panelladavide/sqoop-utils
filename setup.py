39d9122083118e40b9d2a793e855136d7f2870fc
banditopazzo <banditopazzo@gmail.com>
2019-07-23 02:21:33 +0200

    placeholder cli

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(name='sqoop-utils',
       description='Sqoop Utils',
       url='https://github.com/panelladavide/sqoop-utils',
       packages=['sqoop_utils'],
+      entry_points = {
+        'console_scripts': ['sqoop_utils_cli=sqoop_utils.cli:main']
+      },
       test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
---
+++ sqoop_utils/cli/__init__.py
@@ -0,0 +1,5 @@
+def main():
+    print("launched cli")
+
+if __name__ == "__main__":
+    main()
