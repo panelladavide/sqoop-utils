23c970a869d8f1e49b32ab6aee59fd3bc8696db8
banditopazzo <banditopazzo@gmail.com>
2019-07-25 12:30:02 +0200

    solved package data include

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='sqoop-utils',
       version='0.1',
       description='Sqoop Utils',
       url='https://github.com/panelladavide/sqoop-utils',
-      packages=['sqoop_utils'],
+      packages=find_packages(),
+      package_data={'sqoop_utils.cli': ['config_template.json']},
       entry_points = {
         'console_scripts': ['sqoop_utils_cli=sqoop_utils.cli:main']
       },
